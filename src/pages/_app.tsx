5c4574efbbcdccfdf1c9e26621aa92a337235d6e
Udbhav Munipally <udbhav4@gmail.com>
2023-11-14 23:28:31 +0530

    Add files via upload

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from "next/app";
 import ThemeProvider from "@/pages/components/theme-provider";
 import { useTheme } from "next-themes";
 import { RecoilRoot } from "recoil";
-import { SessionProvider } from "next-auth/react";
 import { Toaster } from "@/components/ui/toaster";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -12,15 +11,13 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <ThemeProvider>
-      <SessionProvider>
-        <RecoilRoot>
-          <Component
-            className={`${isDarkTheme ? "text-light" : "text-dark"}`}
-            {...pageProps}
-          />
-          <Toaster />
-        </RecoilRoot>
-      </SessionProvider>
+      <RecoilRoot>
+        <Component
+          className={`${isDarkTheme ? "text-light" : "text-dark"}`}
+          {...pageProps}
+        />
+        <Toaster />
+      </RecoilRoot>
     </ThemeProvider>
   );
 }
